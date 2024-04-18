7a706a8cf12fa3d9728b3506dd001945f7c74cd7
krishnabalusu <155931085+krishnabalusu@users.noreply.github.com>
2024-04-18 06:23:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 from dotenv import load_dotenv
 import pandas as pd
 #import matplotlib.pyplot as plt
+import plotly.express as px
 
 
 
@@ -67,7 +68,6 @@ df_coffee = pd.read_csv("Cleaned.csv")
 # ------------------------------
 # PART 1 : Filter Data
 # ------------------------------
-import plotly.express as px
 
 def load_data(df_coffee):
     try:
