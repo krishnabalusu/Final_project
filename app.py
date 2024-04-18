15b503554079f9d232570aef5e07332d707f2a1c
krishnabalusu <155931085+krishnabalusu@users.noreply.github.com>
2024-04-19 01:19:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -145,3 +145,6 @@ def main():
         plot_sales_performance(sales_performance, country)
         plot_product_distribution(country_df, country)
         plot_sales_trend(country_df, country)
+
+if _name_ == "_main_":
+    main()
